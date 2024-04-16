7626d59ad4e99062088aef07a7b78dcb7d590077
LazaroAlpaka <lazaroalpaka@proton.me>
2024-04-16 11:57:12 +0200

    feat: added websocket and corrected go program

--- client/client/client.go
+++ client/client/client.go
@@ -2,18 +2,38 @@ package client
 
 import (
 	"bufio"
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"net/http"
-	"net/http/httptest"
+	"log"
 	"os"
 	"strings"
+
+	"github.com/gorilla/websocket"
 )
 
 type Input struct {
 	Text      string `json:"text"`
 	SessionID string `json:"token"`
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func handleConnections(ws *websocket.Conn) {
+	for {
+		// Read in a new message as JSON and map it to a Message object
+		var msg Input
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			log.Printf("error: %v", err)
+			break
+		}
+
+		// Print the message to the console
+		log.Printf("%+v\n", msg)
+	}
 }
 
 func contains(slice []string, item string) bool {
@@ -25,36 +45,7 @@ func contains(slice []string, item string) bool {
 	return false
 }
 
-// MOCK FUNCTION PLEASE DELETE
-func mockGetSessionToken(serverURL string) string {
-	// Return a predefined session token
-	return "mockSessionToken"
-}
-
-//REAL GET FUNCTION UNCOMMENT!!
-/*func GetSessionToken(serverURL string) string {
-	// Make a GET request to /connect
-	resp, err := http.Get(serverURL + "/connect")
-	if err != nil {
-		fmt.Println(err)
-		return ""
-	}
-	defer resp.Body.Close()
-
-	// Read the response body
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println(err)
-		return ""
-	}
-
-	// The body contains the session token
-	sessionToken := string(body)
-
-	return sessionToken
-}*/
-
-func sendRequest(text string, serverURL string, sessionToken string) {
+func sendRequest(text string, ws *websocket.Conn, sessionToken string) {
 	// Store the input in a struct
 	input := Input{Text: text, SessionID: sessionToken}
 
@@ -67,48 +58,24 @@ func sendRequest(text string, serverURL string, sessionToken string) {
 	// Print debug to check if the output is correct
 	fmt.Println("JSON to be sent:", string(jsonInput))
 
-	// Create a new request using http
-	req, err := http.NewRequest("GET", serverURL, bytes.NewBuffer(jsonInput))
+	// Send the request via the websocket
+	err = ws.WriteJSON(input)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-
-	// Set the content type header value
-	req.Header.Set("Content-Type", "application/json")
-
-	// Send the request via a client
-	client := &http.Client{}
-	res, err := client.Do(req)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-// Print body of the respsonse (res.body), unmarshal the json and print json.query & json.answer
-	defer res.Body.Close()
-	// Print debug res-code
-	if res.StatusCode != 200 {
-		fmt.Println("Expected status code 200, got ", res.StatusCode)
-	} else {
-		fmt.Println("API call successful!")
-	}
-	fmt.Println(req)
 }
 
 func Initializer() {
-	// Mock server
-	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		// Send response to be tested
-		rw.Write([]byte(`OK`))
-	}))
-	// Close the server when test finishes
-	defer server.Close()
+	// Connect to the WebSocket server
+	ws, _, err := websocket.DefaultDialer.Dial("ws://127.0.0.1:8080/ws", nil)
+	if err != nil {
+		log.Fatal("dial: ", err)
+	}
+	defer ws.Close()
 
-	/*// Get the session token
-	sessionToken := GetSessionToken(server.URL)*/
-
-	// Mock of the above please delete afterwards
-	sessionToken := mockGetSessionToken(server.URL) // Use the mock function here
+	// Mock of the session token
+	sessionToken := "mockSessionToken"
 
 	// loop this and end this with keyword
 	reader := bufio.NewReader(os.Stdin)
@@ -128,7 +95,6 @@ func Initializer() {
 			fmt.Println("Ending the program.")
 			break
 		}
-
-		sendRequest(text, server.URL, sessionToken) // Call the function to send the request
+		sendRequest(text, ws, sessionToken) // Call the function to send the request
 	}
 }
