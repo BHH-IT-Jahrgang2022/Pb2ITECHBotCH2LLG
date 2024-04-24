3c99332ad3e3f48df9e0ee00441d86976e130bc6
R-HOFF <r.hoffbuhr@stud.bhh.de>
2024-04-24 16:47:12 +0200

    feat: Added Mail Pit server to config

--- mailClient/mailClient/mailClient.go
+++ mailClient/mailClient/mailClient.go
@@ -6,9 +6,9 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/smtp"
 	"os"
 	"strings"
-	//"net/smtp"
 )
 
 type Ticket struct {
@@ -16,7 +16,7 @@ type Ticket struct {
 	Problem string   `json:"problem"`
 }
 
-/*func SendEmail(ticket *Ticket) {
+func SendEmail(ticket *Ticket) {
 	to := "botbugland@gmail.com"
 	subject := "New Ticket"
 	body := "Tags: " + strings.Join(ticket.Tags, ", ") + "\nProblem: " + ticket.Problem
@@ -26,11 +26,12 @@ type Ticket struct {
 		body
 
 	// Set up authentication information.
-	smtpHost := "smtp.gmail.com."
-	smtpPort := "587"
+	smtpHost := os.Getenv("SMTPHOST")
+	smtpPort := os.Getenv("SMTPPORT")
 	smtpUser := "botbugland@gmail.com"
 	smtpPass := "DeineMudder123"
 	auth := smtp.PlainAuth("", smtpUser, smtpPass, smtpHost)
+	auth = nil
 
 	// Connect to the server, authenticate, set the sender and recipient,
 	// and send the email all in one step.
@@ -42,19 +43,19 @@ type Ticket struct {
 
 	log.Print("Email sent")
 	fmt.Println("")
-}*/
-
-func SendEmail(ticket *Ticket) {
-	to := "buglandbot@gmail.com"
-	subject := "New Ticket"
-	body := "Tags: " + strings.Join(ticket.Tags, ", ") + "\nProblem: " + ticket.Problem
-
-	msg := "To: " + to + "\n" +
-		"Subject: " + subject + "\n\n" +
-		body
-
-	fmt.Println("Email Content: \n" + msg) // Print the email content to the console
 }
+
+// func SendEmail(ticket *Ticket) {
+// 	to := "buglandbot@gmail.com"
+// 	subject := "New Ticket"
+// 	body := "Tags: " + strings.Join(ticket.Tags, ", ") + "\nProblem: " + ticket.Problem
+
+// 	msg := "To: " + to + "\n" +
+// 		"Subject: " + subject + "\n\n" +
+// 		body
+
+// 	fmt.Println("Email Content: \n" + msg) // Print the email content to the console
+// }
 
 /*func FetchAndEmailTicket() {
 	url := "http://" + os.Getenv("UNSOLVEDHOST") + ":" + os.Getenv("UNSOLVEDPORT") + "/data"
